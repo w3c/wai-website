c53d85f366bce853fdd6ef05de68161cc3a3c3c4
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2024-11-26 14:55:24 -0500

    Switch to using services/meeting-minutes endpoint
    (Does not support scribe v1 output)

--- _plugins/minutes-plugin.rb
+++ _plugins/minutes-plugin.rb
@@ -6,13 +6,29 @@ module Jekyll
   class MinutesFetcher < Generator
     def generate(site)
       start_time = Time.now
-      minutesUri = URI('https://w3.org/WAI/minutes-json.php?start_date=1998-12-01')
+      limit = 2000
+      requested_channels = []
+      site.data['minutes']['groups'].each {|id, channels| requested_channels.push(*channels)}
+
+      minutesUri =
+        URI("https://www.w3.org/services/meeting-minutes?format=json&num=#{limit}&channels=#{requested_channels.uniq().join(',')}")
       data = JSON.parse(Net::HTTP.get(minutesUri))
+      grouped_data = {}
+      data.each do |entry|
+        grouped_data[entry['channel']] = {} unless grouped_data[entry['channel']]
+        grouped_data[entry['channel']]["#{entry['yyyy']}-#{entry['mm']}-#{entry['dd']}"] = {
+          'href' => entry['href'],
+          'title' => entry['title'],
+          'resolutions' => entry['resolutions'],
+          'topics' => entry['topics']
+        }
+      end
+
       site.data['minutes']['groups'].each do |id, channels|
         groupMinutes = []
         for channel in channels do
-          next unless data[channel]
-          data[channel].each do |date, details|
+          next unless grouped_data[channel]
+          grouped_data[channel].each do |date, details|
             groupMinutes.push(details.merge({channel_id: channel, date: date}))
           end
         end
